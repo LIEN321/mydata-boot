4c94d6bf43f3f27ce7ee0f2de087377006880694
Lien <tle3212000@163.com>
2024-05-25 02:24:47 +0800

    feat: 定时更新业务数据量

--- src/main/java/org/springblade/modules/mydata/manage/schedule/ManageSchedule.java
+++ src/main/java/org/springblade/modules/mydata/manage/schedule/ManageSchedule.java
@@ -1,13 +1,20 @@
 package org.springblade.modules.mydata.manage.schedule;
 
+import cn.hutool.core.collection.CollUtil;
 import cn.hutool.core.date.DateUtil;
 import lombok.extern.slf4j.Slf4j;
+import org.springblade.modules.mydata.manage.entity.Data;
+import org.springblade.modules.mydata.manage.entity.Env;
+import org.springblade.modules.mydata.manage.service.IBizDataService;
+import org.springblade.modules.mydata.manage.service.IDataService;
+import org.springblade.modules.mydata.manage.service.IEnvService;
 import org.springblade.modules.mydata.manage.service.ITaskLogService;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.Resource;
 import java.util.Date;
+import java.util.List;
 
 /**
  * 管理模块的定时任务
@@ -22,6 +29,15 @@ public class ManageSchedule {
     @Resource
     private ITaskLogService taskLogService;
 
+    @Resource
+    private IDataService dataService;
+
+    @Resource
+    private IEnvService envService;
+
+    @Resource
+    private IBizDataService bizDataService;
+
     /**
      * 每天凌晨清理 7天前未执行完的日志记录
      */
@@ -30,5 +46,28 @@ public class ManageSchedule {
         Date limitDate = DateUtil.offsetDay(new Date(), -7);
         Date endTime = DateUtil.endOfDay(limitDate);
         taskLogService.deleteUnfinished(endTime);
+    }
+
+    /**
+     * 定时刷新业务数据量
+     */
+    @Scheduled(cron = "0 0 0/4 * * ?")
+    public void refreshBizDataCount() {
+        List<Data> dataList = dataService.list();
+        if (CollUtil.isEmpty(dataList)) {
+            return;
+        }
+
+        dataList.forEach(data -> {
+            Long projectId = data.getProjectId();
+            List<Env> envList = envService.listByProject(projectId);
+            if (CollUtil.isEmpty(envList)) {
+                return;
+            }
+
+            envList.forEach(env -> {
+                bizDataService.updateDataCount(data.getTenantId(), projectId, env.getId(), data.getId());
+            });
+        });
     }
 }
