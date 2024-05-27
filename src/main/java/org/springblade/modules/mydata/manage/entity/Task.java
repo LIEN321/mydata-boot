07e376d0830e57852c5bb846e749e3901fa8e1eb
Lien <tle3212000@163.com>
2024-05-27 13:51:21 +0800

    feat: 任务增加清除html

--- src/main/java/org/springblade/modules/mydata/manage/entity/Task.java
+++ src/main/java/org/springblade/modules/mydata/manage/entity/Task.java
@@ -267,4 +267,9 @@ public class Task extends TenantEntity {
      * 是否复用父任务批次数据，0-不复用、1-复用
      */
     private Integer sameBatch;
+
+    /**
+     * 是否清除HTML标签，0-不清除、1-清除
+     */
+    private Integer cleanHtml;
 }
