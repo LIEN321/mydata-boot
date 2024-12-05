1c2e843b4c5065496fa918a4e36cc37fc389fcb2
LIEN <tle3212000@163.com>
2024-12-05 18:30:16 +0800

    feat: 分页查询数据仓库的业务数据

---
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/manage/controller/BizDataController.java
@@ -0,0 +1,60 @@
+package tech.zhiwei.frostmetal.modules.mydata.manage.controller;
+
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springdoc.core.annotations.ParameterObject;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+import tech.zhiwei.frostmetal.core.base.common.P;
+import tech.zhiwei.frostmetal.core.base.common.PageParam;
+import tech.zhiwei.frostmetal.core.base.common.R;
+import tech.zhiwei.frostmetal.modules.mydata.manage.service.IBizDataService;
+import tech.zhiwei.frostmetal.modules.mydata.manage.service.IDataFieldService;
+import tech.zhiwei.frostmetal.modules.mydata.manage.vo.DataFieldVO;
+import tech.zhiwei.frostmetal.modules.mydata.manage.wrapper.DataFieldWrapper;
+
+import java.util.List;
+import java.util.Map;
+
+/**
+ * 业务数据 控制器
+ *
+ * @author LIEN
+ * @since 2024/12/05
+ */
+@RestController
+@AllArgsConstructor
+@RequestMapping("/bizData")
+@Tag(name = "bizData", description = "标准数据项接口")
+@Slf4j
+public class BizDataController {
+
+    private final IDataFieldService dataFieldService;
+
+    private final IBizDataService bizDataService;
+
+    /**
+     * 根据数据项 查询字段列表
+     *
+     * @param dataId 数据项id
+     * @return 字段列表
+     */
+    @GetMapping("/fieldList")
+    @Operation(summary = "查询标准数据的可见字段列表", operationId = "bizDataFieldList")
+    public R<List<DataFieldVO>> listDataFields(Long dataId) {
+        return R.data(DataFieldWrapper.getInstance().listVO(dataFieldService.listDisplayedFields(dataId)));
+    }
+
+    @GetMapping("/bizDataPage")
+    @Operation(summary = "分页查询业务数据", operationId = "bizDataPage")
+    public P<List<Map<String, Object>>> list(@ParameterObject PageParam pageParam, @RequestParam Long dataId, @RequestParam Map<String, Object> params) {
+        params.remove("current");
+        params.remove("pageSize");
+        params.remove("dataId");
+        return P.page(bizDataService.bizDataPage(pageParam, dataId, params));
+    }
+}
--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/manage/service/IBizDataService.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/manage/service/IBizDataService.java
@@ -1,7 +1,13 @@
 package tech.zhiwei.frostmetal.modules.mydata.manage.service;
 
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import tech.zhiwei.frostmetal.core.base.common.PageParam;
 import tech.zhiwei.frostmetal.core.base.service.IBaseService;
+import tech.zhiwei.frostmetal.modules.mydata.data.BizDataFilter;
 import tech.zhiwei.frostmetal.modules.mydata.manage.entity.Data;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * 业务数据 服务类
@@ -10,10 +16,29 @@ import tech.zhiwei.frostmetal.modules.mydata.manage.entity.Data;
  * @since 2024/12/05
  */
 public interface IBizDataService extends IBaseService<Data> {
+
+    /**
+     * 获取数据项的总数
+     *
+     * @param dbCode   业务数据库名
+     * @param dataCode 业务数据表名
+     * @return 总数
+     */
+    long getTotalCount(String dbCode, String dataCode, List<BizDataFilter> bizDataFilters);
+
     /**
      * 更新指定标准数据的业务数据总量
      *
      * @param dataId 标准数据id
      */
     void updateDataCount(Long dataId);
+
+    /**
+     * 分页查询 业务数据
+     *
+     * @param pageParam 分页
+     * @param dataId    数据标准id
+     * @return 数据列表
+     */
+    IPage<Map<String, Object>> bizDataPage(PageParam pageParam, Long dataId, Map<String, Object> params);
 }
