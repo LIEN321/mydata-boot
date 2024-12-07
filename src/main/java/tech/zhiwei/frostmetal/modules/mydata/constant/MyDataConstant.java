ce2a5b9c623c509c5fab1440c386cdca644a3b9b
LIEN <tle3212000@163.com>
2024-12-07 14:14:40 +0800

    feat: 增加过滤数据任务的常量

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/constant/MyDataConstant.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/constant/MyDataConstant.java
@@ -243,6 +243,11 @@ public interface MyDataConstant {
     String JOB_DATA_KEY_BIZ_DATA = "BIZ_DATA";
 
     /**
+     * Job上下文共享数据的标识：过滤拦截的数据
+     */
+    String JOB_DATA_KEY_FILTER_BLOCKED_DATA = "FILTER_BLOCKED_DATA";
+
+    /**
      * Job上下文共享数据的标识：标准数据的id
      */
     String JOB_DATA_KEY_DATA_ID = "DATA_ID";
