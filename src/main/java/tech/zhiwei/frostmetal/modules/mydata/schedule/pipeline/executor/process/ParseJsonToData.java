55f8f699db7f12709919eb6898b496fb45b2b500
LIEN <tle3212000@163.com>
2024-12-09 16:24:53 +0800

    feat: JSON转业务数据 支持/根目录解析模式

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/process/ParseJsonToData.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/process/ParseJsonToData.java
@@ -5,19 +5,25 @@ import cn.hutool.core.util.StrUtil;
 import cn.hutool.json.JSON;
 import cn.hutool.json.JSONArray;
 import cn.hutool.json.JSONObject;
+import lombok.extern.slf4j.Slf4j;
 import tech.zhiwei.frostmetal.modules.mydata.cache.MyDataCache;
 import tech.zhiwei.frostmetal.modules.mydata.constant.MyDataConstant;
 import tech.zhiwei.frostmetal.modules.mydata.manage.entity.Data;
+import tech.zhiwei.frostmetal.modules.mydata.manage.entity.DataField;
 import tech.zhiwei.frostmetal.modules.mydata.manage.entity.PipelineLog;
 import tech.zhiwei.frostmetal.modules.mydata.manage.entity.PipelineTask;
+import tech.zhiwei.frostmetal.modules.mydata.manage.service.IDataFieldService;
 import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.executor.TaskExecutor;
+import tech.zhiwei.frostmetal.modules.mydata.util.MyDataUtil;
 import tech.zhiwei.tool.collection.CollectionUtil;
 import tech.zhiwei.tool.json.JsonUtil;
 import tech.zhiwei.tool.lang.StringUtil;
 import tech.zhiwei.tool.map.MapUtil;
+import tech.zhiwei.tool.spring.SpringUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * JSON转为业务数据
@@ -25,28 +31,41 @@ import java.util.Map;
  * @author LIEN
  * @since 2024/11/29
  */
+@Slf4j
 public class ParseJsonToData extends TaskExecutor {
+    private final IDataFieldService dataFieldService = SpringUtil.getBean(IDataFieldService.class);
+
     public ParseJsonToData(PipelineTask pipelineTask, PipelineLog pipelineLog) {
         super(pipelineTask, pipelineLog);
     }
 
     @Override
     public void doExecute(Map<String, Object> jobContextData) {
+        // 当前流水线任务
+        PipelineTask pipelineTask = getPipelineTask();
+
         // 输入配置
         Map<String, String> inputMap = getInputMap();
 
         // 获取待解析json的key
-        String inputDataKey = inputMap.get(MyDataConstant.JOB_DATA_KEY_DATA_JSON);
-        if (StringUtil.isEmpty(inputDataKey)) {
+        String dataJsonKey = inputMap.get(MyDataConstant.JOB_DATA_KEY_DATA_JSON);
+        if (StringUtil.isEmpty(dataJsonKey)) {
             error("JSON变量名为空，结束执行。");
             throw new IllegalArgumentException("JSON变量名为空，结束执行。");
         }
 
         // 从上下文获取json
-        List<String> dataJsonList = (List<String>) jobContextData.get(inputDataKey);
+        // 业务数据json
+        List<String> dataJsonList = (List<String>) jobContextData.get(dataJsonKey);
         if (CollectionUtil.isEmpty(dataJsonList)) {
             log("没有JSON待转换，结束执行。");
             return;
+        }
+        // 原始json
+        String originJsonKey = inputMap.get(MyDataConstant.JOB_DATA_KEY_ORIGIN_JSON);
+        List<String> originJsonList = null;
+        if (StringUtil.isNotEmpty(originJsonKey)) {
+            originJsonList = (List<String>) jobContextData.get(originJsonKey);
         }
 
         // 字段映射
@@ -64,10 +83,27 @@ public class ParseJsonToData extends TaskExecutor {
             throw new IllegalArgumentException("输出设置中的业务数据变量名为空，结束执行。");
         }
 
+        // 获取标准数据信息
+        Long dataId = pipelineTask.getDataId();
+        Data data = MyDataCache.getData(dataId);
+        // 标准数据字段列表
+        List<DataField> dataFields = getDataFields(dataId);
+        // 字段编号-字段类型
+        Map<String, String> fieldTypeMapping = dataFields.stream().collect(Collectors.toMap(DataField::getFieldCode, DataField::getFieldType));
+
         // 业务数据集合
         List<Map<String, Object>> bizDataList = CollUtil.newArrayList();
 
-        for (String dataJsonString : dataJsonList) {
+        int size = dataJsonList.size();
+        for (int i = 0; i < size; i++) {
+            String dataJsonString = dataJsonList.get(i);
+            String originJsonString;
+            if (originJsonList != null) {
+                originJsonString = originJsonList.get(i);
+            } else {
+                originJsonString = null;
+            }
+
             log("开始转换JSON：{}", dataJsonString);
             // 业务数据的json对象
             JSON dataJson = JsonUtil.parse(dataJsonString);
@@ -93,12 +129,18 @@ public class ParseJsonToData extends TaskExecutor {
 
                     // 获取业务数据值
                     Object value;
-                    // TODO /field 根目录格式
-//                if (StringUtil.startWith(apiFieldCode, MyDataConstant.FIELD_MAPPING_ROOT)) {
-//                    value = baseJson.getByPath(apiFieldCode.substring(MyDataConstant.FIELD_MAPPING_ROOT.length()));
-//                } else {
-                    value = jsonObject.getByPath(apiFieldCode);
-//                }
+                    // /field 根目录格式
+                    if (StringUtil.startWith(apiFieldCode, MyDataConstant.FIELD_MAPPING_ROOT)) {
+                        if (originJsonString == null) {
+                            error("获取业务数据失败：因失败未配置“原始的JSON”的变量名，字段{} 无法从JSON的接口字段{} 获取数据", dataFieldCode, apiFieldCode);
+                            throw new IllegalArgumentException(StringUtil.format("获取业务数据失败：因失败未配置“原始的JSON”的变量名，字段{} 无法从JSON的接口字段{} 获取数据", dataFieldCode, apiFieldCode));
+                        } else {
+                            JSON originJson = JsonUtil.parse(originJsonString);
+                            value = originJson.getByPath(apiFieldCode.substring(MyDataConstant.FIELD_MAPPING_ROOT.length()));
+                        }
+                    } else {
+                        value = jsonObject.getByPath(apiFieldCode);
+                    }
                     // TODO 未获取到值，再解析属性表达式 从任务变量尝试获取数据
 //                    if (value == null && JobVarService.isFieldExp(apiCode)) {
 //                        value = JobVarService.parseDataFieldVar(apiCode, taskJob.getTaskVar(), taskJob.getFieldTypeMapping());
@@ -108,13 +150,13 @@ public class ParseJsonToData extends TaskExecutor {
                         return;
                     }
 
-//                    String targetType = fieldTypeMapping.get(standardCode);
-//                    try {
-//                        produceData.put(standardCode, MdUtil.convertDataType(value, targetType));
-//                    } catch (Exception e) {
-//                        taskJob.appendLog("转换业务数据出错，数据：{}，字段 {} 转为目标类型 {} 时出错：{}", obj, standardCode, targetType, e.getMessage());
-//                    }
-                    produceData.put(dataFieldCode, value);
+                    String targetType = fieldTypeMapping.get(dataFieldCode);
+                    try {
+                        produceData.put(dataFieldCode, MyDataUtil.convertDataType(value, targetType));
+                    } catch (Exception e) {
+                        error("转换业务数据出错，数据：{}，字段 {} 转为目标类型 {} 时出错：{}", obj, dataFieldCode, targetType, e.getMessage());
+                        throw new RuntimeException(StringUtil.format("转换业务数据出错，数据：{}，字段 {} 转为目标类型 {} 时出错：{}", obj, dataFieldCode, targetType, e.getMessage()));
+                    }
                 });
 
                 // TODO 补充默认字段值
@@ -131,11 +173,6 @@ public class ParseJsonToData extends TaskExecutor {
                 bizDataList.add(produceData);
             });
         }
-
-        // 当前流水线任务
-        PipelineTask pipelineTask = getPipelineTask();
-        // 获取标准数据信息
-        Data data = MyDataCache.getData(pipelineTask.getDataId());
 
         // 数据存入任务上下文数据中
         jobContextData.put(MyDataConstant.JOB_DATA_KEY_BIZ_DATA, bizDataList);
