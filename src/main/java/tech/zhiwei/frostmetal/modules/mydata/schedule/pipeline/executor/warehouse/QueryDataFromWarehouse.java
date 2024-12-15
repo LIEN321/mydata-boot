82fa94f293ba6c98beb90a77271a30b70550d168
LIEN <tle3212000@163.com>
2024-12-15 22:38:20 +0800

    fix: 修复从数仓查询获取的日期 转为json后无法识别问题

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/warehouse/QueryDataFromWarehouse.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/warehouse/QueryDataFromWarehouse.java
@@ -70,6 +70,7 @@ public class QueryDataFromWarehouse extends TaskExecutor {
         log("查询条件：{}", CollectionUtil.emptyIfNull(dataFilters));
         // 查询业务数据
         List<Map<String, Object>> bizDataList = bizDataDAO.list(warehouseName, dataCode, dataFilters);
+        MyDataUtil.parseBizData(bizDataList);
         if (CollectionUtil.isNotEmpty(bizDataList)) {
             bizDataList.forEach(bizData -> {
                 bizData.remove(MyDataConstant.DATA_COLUMN_DATA_ID);
