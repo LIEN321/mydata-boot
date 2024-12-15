ee695ccdaab1d4b58442b374c7ef3f9c4f5df2a5
LIEN <tle3212000@163.com>
2024-12-15 15:13:34 +0800

    fix: 去掉业务数据中的数仓字段

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/warehouse/QueryDataFromWarehouse.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/warehouse/QueryDataFromWarehouse.java
@@ -70,6 +70,12 @@ public class QueryDataFromWarehouse extends TaskExecutor {
         log("查询条件：{}", CollectionUtil.emptyIfNull(dataFilters));
         // 查询业务数据
         List<Map<String, Object>> bizDataList = bizDataDAO.list(warehouseName, dataCode, dataFilters);
+        if (CollectionUtil.isNotEmpty(bizDataList)) {
+            bizDataList.forEach(bizData -> {
+                bizData.remove(MyDataConstant.DATA_COLUMN_DATA_ID);
+                bizData.remove(MyDataConstant.DATA_COLUMN_UPDATE_TIME);
+            });
+        }
 
         log("查询结果：共 {} 条", bizDataList.size());
 
