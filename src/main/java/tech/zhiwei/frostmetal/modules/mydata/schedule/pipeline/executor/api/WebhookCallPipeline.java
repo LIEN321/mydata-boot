8edbb75e106035938dc09847b8ecf670941dee7d
LIEN <tle3212000@163.com>
2024-12-18 00:27:03 +0800

    fix: 若json为空 则不触发指定流水线

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/api/WebhookCallPipeline.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/api/WebhookCallPipeline.java
@@ -1,11 +1,15 @@
 package tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.executor.api;
 
+import cn.hutool.json.JSON;
+import cn.hutool.json.JSONArray;
+import cn.hutool.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import tech.zhiwei.frostmetal.modules.mydata.constant.MyDataConstant;
 import tech.zhiwei.frostmetal.modules.mydata.manage.entity.PipelineLog;
 import tech.zhiwei.frostmetal.modules.mydata.manage.entity.PipelineTask;
 import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.PipelineScheduler;
 import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.executor.TaskExecutor;
+import tech.zhiwei.tool.json.JsonUtil;
 import tech.zhiwei.tool.lang.StringUtil;
 import tech.zhiwei.tool.spring.SpringUtil;
 import tech.zhiwei.tool.util.NumberUtil;
@@ -42,10 +46,23 @@ public class WebhookCallPipeline extends TaskExecutor {
             throw new IllegalArgumentException("JSON变量名为空，结束执行。");
         }
 
-        String json = (String) jobContextData.get(dataJsonKey);
-        Long targetPipelineId = NumberUtil.parseLong((String) getTaskConfig().get("PIPELINE_ID"));
+        String dataJson = (String) jobContextData.get(dataJsonKey);
+        JSON json = JsonUtil.parse(dataJson);
+        if (json instanceof JSONObject jsonObject) {
+            if (jsonObject.isEmpty()) {
+                log("json内容为空，不触发流水线，结束执行。");
+                return;
+            }
+        }
+        if (json instanceof JSONArray jsonArray) {
+            if (jsonArray.isEmpty()) {
+                log("json内容为空，不触发流水线，结束执行。");
+                return;
+            }
+        }
 
-        pipelineScheduler.webhookPipeline(targetPipelineId, json);
-        log("触发流水线成功，提交json={}", json);
+        Long targetPipelineId = NumberUtil.parseLong((String) getTaskConfig().get("PIPELINE_ID"));
+        pipelineScheduler.webhookPipeline(targetPipelineId, dataJson);
+        log("触发流水线成功，提交json={}", dataJson);
     }
 }
