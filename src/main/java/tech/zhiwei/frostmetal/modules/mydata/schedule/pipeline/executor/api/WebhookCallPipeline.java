23f04ce5afc5e603dfadd14a939c9ca41d974cbf
LIEN <tle3212000@163.com>
2024-12-19 19:00:14 +0800

    refactor: 改用工具方法判断json是否为空

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/api/WebhookCallPipeline.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/api/WebhookCallPipeline.java
@@ -1,8 +1,5 @@
 package tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.executor.api;
 
-import cn.hutool.json.JSON;
-import cn.hutool.json.JSONArray;
-import cn.hutool.json.JSONObject;
 import lombok.extern.slf4j.Slf4j;
 import tech.zhiwei.frostmetal.modules.mydata.constant.MyDataConstant;
 import tech.zhiwei.frostmetal.modules.mydata.manage.entity.PipelineLog;
@@ -47,18 +44,9 @@ public class WebhookCallPipeline extends TaskExecutor {
         }
 
         String dataJson = (String) jobContextData.get(dataJsonKey);
-        JSON json = JsonUtil.parse(dataJson);
-        if (json instanceof JSONObject jsonObject) {
-            if (jsonObject.isEmpty()) {
-                log("json内容为空，不触发流水线，结束执行。");
-                return;
-            }
-        }
-        if (json instanceof JSONArray jsonArray) {
-            if (jsonArray.isEmpty()) {
-                log("json内容为空，不触发流水线，结束执行。");
-                return;
-            }
+        if (JsonUtil.isEmpty(dataJson)) {
+            log("json内容为空，不触发流水线，结束执行。");
+            return;
         }
 
         Long targetPipelineId = NumberUtil.parseLong((String) getTaskConfig().get("PIPELINE_ID"));
