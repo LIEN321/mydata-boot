eca071349fba241ad7045e37394b0ff2cbd37704
LIEN <tle3212000@163.com>
2024-12-22 15:36:55 +0800

    feat: 查询数据 增加业务数据输入参数

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/warehouse/QueryDataFromWarehouse.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/warehouse/QueryDataFromWarehouse.java
@@ -11,14 +11,17 @@ import tech.zhiwei.frostmetal.modules.mydata.manage.entity.PipelineLog;
 import tech.zhiwei.frostmetal.modules.mydata.manage.entity.PipelineTask;
 import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.bean.PipelineBizData;
 import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.executor.TaskExecutor;
+import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.service.JobVarService;
 import tech.zhiwei.frostmetal.modules.mydata.util.MyDataUtil;
 import tech.zhiwei.tool.collection.CollectionUtil;
 import tech.zhiwei.tool.lang.ObjectUtil;
 import tech.zhiwei.tool.lang.StringUtil;
+import tech.zhiwei.tool.map.MapUtil;
 import tech.zhiwei.tool.spring.SpringUtil;
 
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 /**
  * 从数据仓库查询业务数据
@@ -37,6 +40,16 @@ public class QueryDataFromWarehouse extends TaskExecutor {
     public void doExecute(Map<String, Object> jobContextData) {
         PipelineTask pipelineTask = getPipelineTask();
 
+        // 输入参数
+        Map<String, String> inputMap = getInputMap();
+        // 获业务数据的key
+        String inputBizDataKey = inputMap.get(MyDataConstant.JOB_DATA_KEY_BIZ_DATA);
+        PipelineBizData inputBizData = null;
+        if (StringUtil.isNotEmpty(inputBizDataKey)) {
+            // 上下文业务数据
+            inputBizData = (PipelineBizData) jobContextData.get(inputBizDataKey);
+        }
+
         Long dataId = pipelineTask.getDataId();
         if (ObjectUtil.isNull(dataId)) {
             error("查询失败：未选择数据标准");
@@ -45,8 +58,8 @@ public class QueryDataFromWarehouse extends TaskExecutor {
 
         // 输出参数
         Map<String, String> outputMap = getOutputMap();
-        String bizDataKey = outputMap.get(MyDataConstant.JOB_DATA_KEY_BIZ_DATA);
-        if (StringUtil.isEmpty(bizDataKey)) {
+        String outputBizDataKey = outputMap.get(MyDataConstant.JOB_DATA_KEY_BIZ_DATA);
+        if (StringUtil.isEmpty(outputBizDataKey)) {
             error("查询失败：无效的输出设置，未配置查询结果的变量名");
             throw new RuntimeException("查询失败：无效的输出设置，未配置查询结果的变量名");
         }
@@ -54,6 +67,27 @@ public class QueryDataFromWarehouse extends TaskExecutor {
         // 查询条件
         List<Map<String, Object>> dataFilterConfig = (List<Map<String, Object>>) pipelineTask.getTaskConfig().get("DATA_FILTER");
         List<BizDataFilter> dataFilters = convertBizDataFilter(dataFilterConfig);
+        dataFilters = CollectionUtil.emptyIfNull(dataFilters);
+
+        // 处理查询条件中的上下文变量
+        if (inputBizData != null) {
+            if (CollectionUtil.isEmpty(inputBizData.getBizData())) {
+                log("没有业务数据可作为参数，结束执行");
+                return;
+            } else {
+                Map<String, Object> bizDataMap = MapUtil.newHashMap();
+                // 字段编号-字段类型
+                Map<String, String> fieldTypeMapping = inputBizData.getDataFields().stream().collect(Collectors.toMap(DataField::getFieldCode, DataField::getFieldType));
+                bizDataMap.putAll(inputBizData.getBizData().get(0));
+
+                dataFilters.forEach(filter -> {
+                    if (JobVarService.isFieldExp(filter.getValue().toString())) {
+                        String filterValue = JobVarService.processDataFieldVar(filter.getValue().toString(), bizDataMap, fieldTypeMapping);
+                        filter.setValue(filterValue);
+                    }
+                });
+            }
+        }
 
         // 标准数据
         Data data = MyDataCache.getData(dataId);
@@ -64,7 +98,8 @@ public class QueryDataFromWarehouse extends TaskExecutor {
         String dataCode = data.getDataCode();
 
         log("开始查询数据：{}", data.getDataName());
-        log("查询条件：{}", CollectionUtil.emptyIfNull(dataFilters));
+
+        log("查询条件：{}", dataFilters);
         // 查询业务数据
         List<Map<String, Object>> bizDataList = bizDataDAO.list(warehouseName, dataCode, dataFilters);
         MyDataUtil.processBizData(bizDataList);
@@ -75,8 +110,8 @@ public class QueryDataFromWarehouse extends TaskExecutor {
         List<DataField> dataFields = getDataFields(dataId);
         // 输出参数
 //        jobContextData.put(bizDataKey, bizDataList);
-        PipelineBizData pipelineBizData = new PipelineBizData(dataId, dataCode, dataFields, bizDataList);
-        jobContextData.put(bizDataKey, pipelineBizData);
+        PipelineBizData outputBizData = new PipelineBizData(dataId, dataCode, dataFields, bizDataList);
+        jobContextData.put(outputBizDataKey, outputBizData);
     }
 
     private List<BizDataFilter> convertBizDataFilter(List<Map<String, Object>> dataFilterList) {
