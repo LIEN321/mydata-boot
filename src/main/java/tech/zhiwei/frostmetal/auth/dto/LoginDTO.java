4c78b012bbe87fbf208ed9c0001df637b06c60e4
LIEN <tle3212000@163.com>
2025-01-03 15:16:46 +0800

    fix: 登录DTO增加不为空的校验

--- src/main/java/tech/zhiwei/frostmetal/auth/dto/LoginDTO.java
+++ src/main/java/tech/zhiwei/frostmetal/auth/dto/LoginDTO.java
@@ -1,6 +1,7 @@
 package tech.zhiwei.frostmetal.auth.dto;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import jakarta.validation.constraints.NotEmpty;
 import lombok.Data;
 
 /**
@@ -13,11 +14,17 @@ import lombok.Data;
 @Schema(description = "登录参数")
 public class LoginDTO {
     @Schema(description = "租户编号")
+    @NotEmpty(message = "租户编号不能为空")
     private String code;
+
     @Schema(description = "登录账号")
+    @NotEmpty(message = "登录账号不能为空")
     private String username;
+
     @Schema(description = "登录密码")
+    @NotEmpty(message = "登录密码不能为空")
     private String password;
+
     @Schema(description = "自动登录")
     private Boolean autoLogin = false;
 }
