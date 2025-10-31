ab9dd3faae3e112d82f6ddf073ae9422831224e8
LIEN <tle3212000@163.com>
2025-10-31 16:32:24 +0800

    tmp: APP、API增加resp_config字段

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/manage/dto/AppApiDTO.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/manage/dto/AppApiDTO.java
@@ -59,4 +59,7 @@ public class AppApiDTO extends BaseDTO {
 
     @Schema(description = "数据结构模式，1-对象、2-集合")
     private Integer dataMode;
+
+    @Schema(description = "响应配置")
+    private Map<String, Object> respConfig;
 }
