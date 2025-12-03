404c30125d4c05447d241c40306f98d002d5ea65
LIEN <tle3212000@163.com>
2025-12-03 13:36:04 +0800

    feat: 发送邮件的主题、内容 支持${var}表达式获取上下文值

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/email/SendEmail.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/email/SendEmail.java
@@ -3,6 +3,7 @@ package tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.executor.email;
 import tech.zhiwei.frostmetal.modules.mydata.mail.MailSender;
 import tech.zhiwei.frostmetal.modules.mydata.manage.entity.PipelineTask;
 import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.executor.TaskExecutor;
+import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.service.JobVarService;
 import tech.zhiwei.tool.lang.StringUtil;
 import tech.zhiwei.tool.map.MapUtil;
 
@@ -40,12 +41,14 @@ public class SendEmail extends TaskExecutor {
 //            error("发送邮件失败，邮件主题无效");
             throw new IllegalArgumentException("发送邮件失败，邮件主题无效");
         }
+        JobVarService.processDataFieldVar(subject, jobContextData);
 
         String content = emailConfig.get("CONTENT");
         if (StringUtil.isEmpty(content)) {
 //            error("发送邮件失败，邮件内容无效");
             throw new IllegalArgumentException("发送邮件失败，邮件内容无效");
         }
+        JobVarService.processDataFieldVar(content, jobContextData);
 
         String fileKey = emailConfig.get("FILE");
         File file = null;
