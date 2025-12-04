3923555187e869882daa2fdee1c1738103d6d6e8
LIEN <tle3212000@163.com>
2025-12-04 12:20:39 +0800

    feat: 设置变量值 支持变量表达式和运算表达式

--- src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/var/SetPipelineVar.java
+++ src/main/java/tech/zhiwei/frostmetal/modules/mydata/schedule/pipeline/executor/var/SetPipelineVar.java
@@ -1,7 +1,9 @@
 package tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.executor.var;
 
+import cn.hutool.extra.expression.ExpressionUtil;
 import tech.zhiwei.frostmetal.modules.mydata.constant.MyDataConstant;
 import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.executor.TaskExecutor;
+import tech.zhiwei.frostmetal.modules.mydata.schedule.pipeline.service.JobVarService;
 import tech.zhiwei.frostmetal.modules.mydata.util.MyDataUtil;
 import tech.zhiwei.tool.collection.CollectionUtil;
 import tech.zhiwei.tool.lang.StringUtil;
@@ -33,7 +35,16 @@ public class SetPipelineVar extends TaskExecutor {
             String varCode = varMapping.get("varCode");
             String varType = StringUtil.nullToDefault(varMapping.get("varType"), MyDataConstant.DATA_TYPE_STRING);
             if (StringUtil.isNotEmpty(varCode)) {
-                Object varValue = MyDataUtil.convertDataType(varMapping.get("varValue"), varType);
+                // 替换${var.property}表达式的值
+                Object varValue = JobVarService.processDataFieldVar(varMapping.get("varValue"), jobContextData);
+                // 支持运算表达式
+                try {
+                    varValue = ExpressionUtil.eval(varValue.toString(), jobContextData);
+                } catch (Exception e) {
+                    // 忽略表达式解析异常，普通字符串、日期等内容不计算
+                }
+                // 转为指定类型
+                varValue = MyDataUtil.convertDataType(varValue, varType);
                 jobContextData.put(varCode, varValue);
                 log("设置变量 {} = {}", varCode, varValue);
             }
